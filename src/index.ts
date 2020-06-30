19de1e00601bc9aa10a928c83e57d984275ae210
温兴杰 <wenxingjie@xiao100.com>
2020-06-30 19:59:26 +0800

    feat: 简单创建axios函数

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { axiosRequestConfig } from './types/index'
+function axios(config: axiosRequestConfig) {
+  // TODO
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'head'
+  | 'HEAD'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+  | 'delete'
+  | 'DELETE'
+  | 'options'
+  | 'OPTIONS'
+export interface axiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
