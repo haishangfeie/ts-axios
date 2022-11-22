befe97a8d8ac833bc61f6ab6d6ab37c318fae9b9
wenxingjie <26127631+haishangfeie@users.noreply.github.com>
2022-11-23 03:32:28 +0800

    fix(Axios.ts): should treat method value as lowercase string

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -29,6 +29,7 @@ export default class Axios {
     }
 
     config = mergeConfig(this.defaults, config)
+    config.method = config.method.toLowerCase()
 
     const chain: PromiseChain<any>[] = [
       {
